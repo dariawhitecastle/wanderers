793ae22cb12c6abee7bc6e741cbd5fd9e837d0bb
DariaDesiderata <daria.calwell@gmail.com>
2017-07-05 12:29:47 -0600

    style updates

--- public/scripts/controls.js
+++ public/scripts/controls.js
@@ -4,19 +4,14 @@ let mobileMode = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|
 function initializeControls(camera, element) {
 
   if (mobileMode) {
-    // solarSysControls = new THREE.DeviceOrientationControls( camera, element )
-    solarSysControls = new THREE.FlyControls(camera, element)
-    solarSysControls.autoForward = false
-    solarSysControls.dragToLook = true
-    solarSysControls.movementSpeed = 10
-    solarSysControls.rollSpeed = 10
-    window.addEventListener('deviceorientation', setOrientationControls, true)
-    console.log("is Mobile!");
+    solarSysControls = new THREE.DeviceOrientationControls( camera, element )
+    // solarSysControls = new THREE.FlyControls(camera)
+    // solarSysControls.autoForward = false
+    
 	} else {
     // WASD-style movement controls
     solarSysControls = new THREE.OrbitControls(camera, element)
     window.addEventListener('deviceorientation', setOrientationControls, true)
-    console.log("is not Mobile!")
   }
 }
 
